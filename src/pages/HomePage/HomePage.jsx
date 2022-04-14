6f70f051a1ae739f5ef5177e83a5d2825d35937a
andyphl <phlai951016@gmail.com>
2022-04-14 19:14:37 +0800

    feat: Adjust some homepage style

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,10 @@
 import { MapChart } from "../../components";
 import styled from "./HomePage.module.scss";
+import classNames from "classnames";
 
 const HomePage = () => {
   return (
-    <div className={styled.homePage}>
+    <div className={classNames("bg-secondary bg-gradient", styled.homePage)}>
       <MapChart />
     </div>
   );
